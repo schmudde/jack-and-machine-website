d1d07b82a84a953b774c7feb7ee6d1bacb88ebf5
schmudde <schmudde@beyondthefra.me>
2015-06-14 01:25:12 +0200

    Footer placement for mobile update

--- global/js/global.js
+++ global/js/global.js
@@ -1,7 +1,17 @@
+
+// Bootstrap defaults
+var screen_sm_min = 768;
+var screen_md_min = 992;
+var screen_lg_min = 1200;
+
 (function() {
     function contentHeightResizer() {
-        var footerHeight = $("#frontPageFooter").height() + 45;
-        $("#frontPage").css("padding-bottom", footerHeight);
+	if ($(window).width() >= screen_sm_min) {
+            var footerHeight = $("#frontPageFooter").height() + 45;
+            $(".mainContent").css("padding-bottom", footerHeight);
+	} else {
+	    $(".mainContent").css("padding-bottom", 15);
+	}
     }
 
     $(document).on("ready", contentHeightResizer);
